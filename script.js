fa7a0064ec3ccf99f7fcfe14cefc04650e38cfab
rhkfb1113 <rhkfb1113@gmail.com>
2020-01-09 20:30:23 +0900

    Update

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-var age = prompt('How old are you?');
+var todos = [];
 
-if (age < 0) {
-  alert('Error');
-} else if (age == 21) {
-  alert('happy 21st birthday!!');
-} else if (age % 2 === 1) {
-  alert('your age is odd!');
-} else if (age % Math.sqrt(age) === 0) {
-  alert('Perfect sqaure!');
+var input = prompt('What would you like to do?');
+
+while (input !== 'quit') {
+  // Handle input
+  if (input === 'list') {
+    console.log(todos);
+  } else if (input === 'new') {
+    // Ask for new todo
+    var newTodo = prompt('Enter new todo');
+    // Add to todos array
+    todos.push(newTodo);
+  }
+  // Ask again for new input
+  input = prompt('What would you like to do?');
 }
+console.log('Ok, You quit the app.');
