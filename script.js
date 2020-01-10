559976892af1dc8246e6cb51b94d7eaa29324881
rhkfb1113 <rhkfb1113@gmail.com>
2020-01-11 01:27:32 +0900

    Upadate

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-var todos = [];
+var p = document.getElementsByTagName('p')[0];
+var ul = document.querySelector('ul');
 
-var input = prompt('What would you like to do?');
-
-while (input !== 'quit') {
-  // Handle input
-  if (input === 'list') {
-    console.log(todos);
-  } else if (input === 'new') {
-    // Ask for new todo
-    var newTodo = prompt('Enter new todo');
-    // Add to todos array
-    todos.push(newTodo);
-  }
-  // Ask again for new input
-  input = prompt('What would you like to do?');
-}
-console.log('Ok, You quit the app.');
+p.textContent = 'uhahaha';
