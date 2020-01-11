d81a95352b6b8eb260b6ff9366182d9c7edf044b
rhkfb1113 <rhkfb1113@gmail.com>
2020-01-12 01:07:44 +0900

    Update

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-var p = document.getElementsByTagName('p')[0];
-var ul = document.querySelector('ul');
+var p1Button = document.querySelector('#p1');
+var p2Button = document.querySelector('#p2');
+var resetButton = document.querySelector('#reset');
+var p1Display = document.querySelector('#p1Display');
+var p2Display = document.querySelector('#p2Display');
+var numInput = document.querySelector('input');
+var winningScoreDisplay = document.querySelector('p span');
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
-p.textContent = 'uhahaha';
+p1Button.addEventListener('click', function() {
+  if (!gameOver) {
+    p1Score++;
+    p1Display.textContent = p1Score;
+    if (p1Score === winningScore) {
+      p1Display.classList.add('winner');
+      gameOver = true;
+    }
+  }
+});
+
+p2Button.addEventListener('click', function() {
+  if (!gameOver) {
+    p2Score++;
+    p2Display.textContent = p2Score;
+    if (p2Score === winningScore) {
+      p2Display.classList.add('winner');
+      gameOver = true;
+    }
+  }
+});
+
+resetButton.addEventListener('click', function() {
+  reset();
+});
+
+function reset() {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+  p1Display.classList.remove('winner');
+  p2Display.classList.remove('winner');
+  gameOver = false;
+}
+
+numInput.addEventListener('change', function() {
+  winningScoreDisplay.textContent = this.value;
+  winningScore = Number(this.value);
+  reset();
+});
